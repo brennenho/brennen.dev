e04b0a299ef0ea65f0f6a33a06a81f3c2bf29867
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2025-01-10 10:52:51 -0800

    component cleanup

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ export * from "./hero";
 export * from "./icons";
 export * from "./menu";
 export * from "./scroll-arrow";
-export * from "./section";
 export * from "./social-icons";
 export * from "./theme-provider";
 export * from "./theme-toggle";
