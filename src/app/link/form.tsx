99d901d15895fb43ec056ad0afe2b28884c28ede
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2025-01-15 20:18:04 -0800

    add shad select and update link form

--- src/app/link/form.tsx
+++ src/app/link/form.tsx
@@ -12,11 +12,17 @@ import {
   FormItem,
   FormMessage,
   Input,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "~/components/ui";
 
 const formSchema = z.object({
   short: z.string().min(1).max(30).optional(),
   target: z.string().url(),
+  expiresAt: z.string(),
 });
 
 export function LinkForm() {
@@ -25,24 +31,25 @@ export function LinkForm() {
     defaultValues: {
       short: "",
       target: "",
+      expiresAt: "never",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data);
   }
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex w-3/4 flex-row justify-center gap-4"
+        className="flex w-3/4 flex-col items-center gap-4 md:flex-row md:items-start"
       >
         <FormField
           control={form.control}
           name="short"
           render={({ field }) => (
-            <FormItem className="w-1/2">
+            <FormItem className="w-full md:w-1/2">
               <FormControl>
                 <Input placeholder="short link" {...field} />
               </FormControl>
@@ -57,10 +64,34 @@ export function LinkForm() {
           control={form.control}
           name="target"
           render={({ field }) => (
-            <FormItem className="w-1/2">
+            <FormItem className="w-full md:w-1/2">
               <FormControl>
                 <Input placeholder="target link" {...field} />
               </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="expiresAt"
+          render={({ field }) => (
+            <FormItem className="w-full md:w-1/3">
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="select duration" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="never">never</SelectItem>
+                  <SelectItem value="24 hours">24 hours</SelectItem>
+                  <SelectItem value="7 days">7 days</SelectItem>
+                  <SelectItem value="30 days">30 days</SelectItem>
+                  <SelectItem value="12 months">12 months</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormDescription>Link expiration</FormDescription>
               <FormMessage />
             </FormItem>
           )}
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,6 +4,7 @@ export * from "./input";
 export * from "./label";
 export * from "./navigation-menu";
 export * from "./scroll-area";
+export * from "./select";
 export * from "./separator";
 export * from "./skeleton";
 export * from "./sonner";
