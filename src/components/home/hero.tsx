df9fe927291a7a10c490fbd236d2ede8f69f9d93
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2025-01-26 16:39:38 -0800

    redo hero design

---
+++ src/components/home/hero.tsx
@@ -0,0 +1,34 @@
+import ProfilePic from "$/img/bho.jpg";
+import Image from "next/image";
+import { SocialIcons } from "~/components";
+import { ScrollArrow } from "./scroll-arrow";
+
+export function Hero() {
+  return (
+    <div className="relative flex h-screen w-full flex-col-reverse items-center justify-center gap-6 pt-16 md:flex-row md:justify-between md:px-[10vw]">
+      <div className="flex -translate-y-16 flex-col gap-6 md:-translate-y-32">
+        <div className="flex flex-col gap-2 text-center md:text-left">
+          <h1 className="text-4xl font-bold md:text-5xl">
+            Hey, I'm <span className="text-primary">Brennen</span>
+          </h1>
+          <div className="text-lg md:text-xl">
+            Impact-driven engineer excited about responsible AI.
+          </div>
+        </div>
+        <SocialIcons className="h-6 w-6 md:h-7 md:w-7" />
+      </div>
+
+      <div className="flex-shrink-0 -translate-y-16 md:-translate-y-32">
+        <Image
+          src={ProfilePic}
+          alt="Brennen Ho"
+          className="h-32 w-32 rounded-full border-4 border-foreground object-cover md:h-48 md:w-48"
+        />
+      </div>
+      <ScrollArrow
+        targetId="scroll-target"
+        className="absolute bottom-8 left-1/2 -translate-x-1/2"
+      />
+    </div>
+  );
+}
