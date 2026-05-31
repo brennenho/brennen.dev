25aed410362627b5c593272e2c20b7fbf7c147c6
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2026-05-30 21:53:15 -0700

    feat: add explicit metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,33 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://brennen.dev"),
   title: "Brennen Ho",
   description:
     "I create intuitive products that simplify, accelerate, and personalize — with an emphasis on applied AI.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  openGraph: {
+    title: "Brennen Ho",
+    description:
+      "I create intuitive products that simplify, accelerate, and personalize — with an emphasis on applied AI.",
+    url: "https://brennen.dev",
+    siteName: "Brennen Ho",
+    images: [
+      {
+        url: "/favicon.ico",
+        width: 2400,
+        height: 2400,
+        alt: "Brennen Ho",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary",
+    title: "Brennen Ho",
+    description:
+      "I create intuitive products that simplify, accelerate, and personalize — with an emphasis on applied AI.",
+    images: ["/favicon.ico"],
+  },
 };
 
 export default function RootLayout({
